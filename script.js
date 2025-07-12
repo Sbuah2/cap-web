ff6e85a8db4f44528b8fc0992e2f2a780c8823bd
Sbuah2 <fmm3551@gmail.com>
2025-07-12 21:58:22 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = 'https://bspnujyvwliymghabrcq.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9';
 const { createClient } = supabase;
 const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
